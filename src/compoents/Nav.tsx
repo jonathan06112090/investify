515fc8c215a74c3ad7db159281c7c54a8610e906
jonathan06112090 <120277969+jonathan06112090@users.noreply.github.com>
2024-06-08 14:29:17 +0200

    Update Nav.tsx

--- src/compoents/Nav.tsx
+++ src/compoents/Nav.tsx
@@ -4,6 +4,9 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 import Link from 'next/link';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import { useState } from 'react';
+import { ArrowRight } from 'lucide-react';
+import { buttonVariants } from './ui/button';
+
 
 
 export default function Nav() {
@@ -16,6 +19,8 @@ export default function Nav() {
   const closeDropdown = () => {
     setIsDropdownOpen(false);
   };
+
+  
 
   const { user, error, isLoading } = useUser();
 
@@ -34,49 +39,82 @@ export default function Nav() {
                     className='object-none rounded-3xl '
                   />
             </Link>
-            <button
-            className="hover:text-black transition duration-300 ease-in-out cursor-pointer flex items-center relative"
-            onClick={toggleDropdown}>
-               Button
-              <svg width="20" height="20" viewBox="28 28" focusable="false" className="NMm5M">
-                <path d="M7 10l5 5 5-5H7z"></path>
-              </svg>
-            {isDropdownOpen && (
-              <div className="absolute top-14 left-0 w-40 bg-white rounded-md shadow-lg transition-opacity duration-300 ease-in-out opacity-100">
-                <div className="py-1">
-                  <Link href="/ok">
-                  <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
-                  A
-                  </span>
-                </Link>
-                <Link href="/ok2">
-                  <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
-                  B
-                  </span>
-                </Link>
-            {/* Add more dropdown links */}
-              </div>
-            </div>
-            )}
-            </button>
+            
             {/* todo: add mobile navbar */}
+            
             {user ? (
               <div className='flex items-center space-x-4 md:space-x-8 mt-4 md:mt-0'>
+                <button className="hover:text-black transition duration-300 ease-in-out cursor-pointer flex items-center relative"  onClick={toggleDropdown}>
+                  Button
+                  <svg width="20" height="20" viewBox="28 28" focusable="false" className="NMm5M">
+                  <path d="M7 10l5 5 5-5H7z"></path>
+                </svg>
+                {isDropdownOpen && (
+                <div className="absolute top-14 left-0 w-40 bg-white rounded-md shadow-lg transition-opacity duration-300 ease-in-out opacity-100">
+                  <div className="py-1">
+                    <Link href="/ok">
+                    <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
+                      A
+                    </span>
+                    </Link>
+                    <Link href="/ok2">
+                    <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
+                      B
+                      </span>
+                      </Link>
+                  </div>
+                </div>
+                )}
+                </button>
                 <div className='hover:text-black transition duration-300 ease-in-out cursor-pointer'><Link href="/NyProfile">{user.name}</Link></div>
+                <div className='hover:text-black transition duration-300 ease-in-out cursor-pointer'><Link href="/FAQ">FAQ</Link></div>
+                <div className='hover:text-black transition duration-300 ease-in-out cursor-pointer'><Link href="/pricing">Pris</Link></div>
                 <Link href="/api/auth/logout">
                   Logout
                 </Link>
               </div>
             ) : (
-              <Link href="/api/auth/login">
-                Login
-              </Link>
+              <div  className='flex items-center space-x-4 md:space-x-8 mt-4 md:mt-0'>
+                <button className="hover:text-black transition duration-300 ease-in-out cursor-pointer flex items-center relative"  onClick={toggleDropdown}>
+                  Button
+                  <svg width="20" height="20" viewBox="28 28" focusable="false" className="NMm5M">
+                  <path d="M7 10l5 5 5-5H7z"></path>
+                </svg>
+                {isDropdownOpen && (
+                <div className="absolute top-14 left-0 w-40 bg-white rounded-md shadow-lg transition-opacity duration-300 ease-in-out opacity-100">
+                  <div className="py-1">
+                    <Link href="/ok">
+                    <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
+                      A
+                    </span>
+                    </Link>
+                    <Link href="/ok2">
+                    <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
+                      B
+                      </span>
+                      </Link>
+                  </div>
+                </div>
+                )}
+                </button>
+                <div className='hover:text-black transition duration-300 ease-in-out cursor-pointer'><Link href="/FAQ">FAQ</Link></div>
+                <div className='hover:text-black transition duration-300 ease-in-out cursor-pointer'><Link href="/pricing">Pris</Link></div>
+                
+                <Link href="/api/auth/login">
+                  Login
+                </Link>
+                <div className='hover:text-black transition duration-300 ease-in-out cursor-pointer'>
+                  <Link className={buttonVariants()} href='/porfolio' target='_blank'>
+                    Kom igang  <ArrowRight className='ml-2 h-5 w-5'/>
+                  </Link>
+                </div>
+              </div>
             )}
           </div>
         </MaxWidthWrapper>
       </nav>
       <div className="sm:flex hidden fixed bottom-0 left-0 text-teal-600"> {/* on small devices it's to be flex (meaning visible) but usually it's going to be hidden */}
-        &copy;Investify A/S
+        &copy;JHC 
       </div>
     </>
   );
