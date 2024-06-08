f3ac21259f941988bae73916e5b69d277ae7010f
jonathan06112090 <120277969+jonathan06112090@users.noreply.github.com>
2024-06-08 20:21:21 +0200

    Update page.tsx

--- src/app/aktier/page.tsx
+++ src/app/aktier/page.tsx
@@ -40,7 +40,7 @@ export default async function Page() {
                 <div className='-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4'>
                 <Image
                     src='/aktierMidlerTidigt.JPG'
-                    alt='LOGO'
+                    alt='Aktier Midler Tidigt'
                     width={1526}
                     height={1341}
                     className='rounded-md bg-white p-2 sm:p-8 md:p-20 shadow-2xl ring-1 ring-gray-900/10'
