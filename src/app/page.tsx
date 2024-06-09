171c154ca9e2e17a273e1ff6311c6056f5cf9a4c
jonathan06112090 <120277969+jonathan06112090@users.noreply.github.com>
2024-06-09 17:35:57 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,7 @@ export default function Home() {
                  />
     <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
         <p className='text-sm font-semibold text-gray-700'>
-            Investify i pre-beta-stadiet
+            Investify i Beta-stadiet
         </p>
     </div>
     
