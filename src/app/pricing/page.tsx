2381a2b814a2f1fb08da597a620be8cdff8e2c25
jonathan06112090 <120277969+jonathan06112090@users.noreply.github.com>
2024-06-10 20:15:04 +0200

    Update page.tsx

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -3,7 +3,7 @@ import { PLANS } from "@/config/stripe";
 import { cn } from "@/lib/utils";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/compoents/ui/tooltip";
 import React from "react";
-import { HelpCircle } from "lucide-react";
+import { Check, HelpCircle, Minus } from "lucide-react";
 /*https://www.youtube.com/watch?v=ucX2zXAZ1I0 9:06:45 and stripe config  https://docs.stripe.com/cli/config
              
              */
@@ -11,78 +11,85 @@ import { HelpCircle } from "lucide-react";
 const pricingItems = [
   {
     plan: 'Gratis',
-    tagline: 'For small side projects.',
-    quota: 10,
+    tagline: 'Til små sideprojekter.',
     features: [
       {
-        text: '5 pages per PDF',
-        footnote: 'The maximum amount of pages per PDF-file.',
+        text: 'Aktiekurser',
+        footnote: 'Kan se aktiekurser',
       },
       {
-        text: '4MB file size limit',
-        footnote: 'The maximum file size of a single PDF file.',
+        text: 'Afkastberegner',
+        footnote: 'adgang til afkastberegner',
       },
       {
-        text: 'Mobile-friendly interface',
-      },
-      {
-        text: 'Higher-quality responses',
-        footnote: 'Better algorithmic responses for enhanced content quality',
+        text: 'portefølje oversigt/hjælper',
+        footnote: 'ok',
         negative: true,
       },
       {
-        text: 'Priority support',
+        text: 'AI-forslag',
+        footnote: 'AI-forslag baseret på præference',
+        negative: true,
+      },
+      {
+        text: 'Live nyhedsopdateringer',
+        footnote: 'Live nyhedsopdateringer releterede til aktier',
         negative: true,
       },
     ],
   },
   {
     plan: 'Pro',
-    tagline: 'For larger projects with higher needs.',
+    tagline: 'Til en investor ',
     quota: PLANS.find((p) => p.slug === 'pro')!,
     features: [
       {
-        text: '25 pages per PDF',
-        footnote: 'The maximum amount of pages per PDF-file.',
+        text: 'Aktiekurser',
+        footnote: 'Kan se aktiekurser',
       },
       {
-        text: '16MB file size limit',
-        footnote: 'The maximum file size of a single PDF file.',
+        text: 'Afkastberegner',
+        footnote: 'adgang til afkastberegner',
       },
       {
-        text: 'Mobile-friendly interface',
+        text: 'portefølje oversigt/hjælper',
+        footnote: 'ok',
       },
       {
-        text: 'Higher-quality responses',
-        footnote: 'Better algorithmic responses for enhanced content quality',
+        text: 'AI-forslag',
+        footnote: 'AI-forslag baseret på præference',
       },
       {
-        text: 'Priority support',
+        text: 'Live nyhedsopdateringer',
+        footnote: 'Live nyhedsopdateringer releterede til aktier',
+        negative: true,
       },
     ],
   },
   {
-    plan: 'Pro+',
-    tagline: 'For larger projects with higher needs.',
+    plan: 'Pro News+',
+    tagline: 'For larger projects with higher needs.',//"Du har med ProNews+ adgang til aktiekurser, afkastberegner, portefølje oversigt/hjælper og AI-forslag baseret på præference og eventuelle live nyhedsopdateringer releterede til aktier."
     quota: PLANS.find((p) => p.slug === 'pro')!,
     features: [
       {
-        text: '25 pages per PDF',
-        footnote: 'The maximum amount of pages per PDF-file.',
+        text: 'Aktiekurser',
+        footnote: 'Kan se aktiekurser',
       },
       {
-        text: '16MB file size limit',
-        footnote: 'The maximum file size of a single PDF file.',
+        text: 'Afkastberegner',
+        footnote: 'adgang til afkastberegner',
       },
       {
-        text: 'Mobile-friendly interface',
+        text: 'portefølje oversigt/hjælper',
+        footnote: 'ok',
       },
       {
-        text: 'Higher-quality responses',
-        footnote: 'Better algorithmic responses for enhanced content quality',
+        text: 'AI-forslag',
+        footnote: 'AI-forslag baseret på præference',
       },
       {
-        text: 'Priority support',
+        text: 'Live nyhedsopdateringer',
+        footnote: 'Live nyhedsopdateringer releterede til aktier',
       },
     ],
   },
@@ -122,7 +129,7 @@ export default function Page() {
                     Opgrader nu
                   </div>
                 )} 
-                {plan === "Pro+" && ( //if plan = Pre
+                {plan === "Pro News+" && ( //if plan = Pro News+
                   <div className='absolute -top-5 left-0 right-0 mx-auto w32 rounded-full bg-gradient-to-r from-green-600 to-cyan-600 px-3 py-2 text-sm font-medium text-white'>
                     Opgrader nu
                   </div>
@@ -142,17 +149,58 @@ export default function Page() {
                 <div className="flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50 rounded-b-xl ">
                   <div className="flex items-center space-x-1">
                    {/*converting the number to the local formatting */}
-
+                    
                     <Tooltip delayDuration={300}>
                       <TooltipTrigger className="cursor-default ml-1.5"> 
+                       
                         <HelpCircle className="h-4 w-4 text-zinc-500"/>
                       </TooltipTrigger>
+                      
                       <TooltipContent className="w-80 p-2">
                         Et Examplegit 
                       </TooltipContent>
                     </Tooltip>
                   </div>
                 </div>
+
+                <ul className="my-10 space-y-5 px-8">
+                  {features.map(({text, footnote, negative}) => (
+                    <li key={text} className="flex space-x-5">
+                      <div className="flex-shrink-0">
+                          {negative ? (
+                            <Minus className="h-6 w-6 text-gray-300"/>
+                          ) : (
+                            <Check className="h-6 w-6 text-gray-500"/>
+                          )}
+                      </div>
+                      {footnote ? (
+                        <div className="flex items-center space-x-1">
+                          <p className={cn("text-gray-400",{"text-gray-600":negative,})}>
+                            {text}
+                            <Tooltip delayDuration={300}>
+                              <TooltipTrigger className="cursor-default ml-1.5"> 
+                        
+                                <HelpCircle className="h-4 w-4 text-zinc-500"/>
+                              </TooltipTrigger>
+                      
+                              <TooltipContent className="w-80 p-2">
+                                {footnote}
+                              </TooltipContent>
+                            </Tooltip>
+                          </p>
+                        </div>
+                      ) : <p>
+                        <p className={cn("text-gray-400",{"text-gray-600":negative,})}></p>
+                        </p>}
+                    </li>
+                  ))}
+
+
+                </ul>
+
+
+
+
               </div>
               )
              }
