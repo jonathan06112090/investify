d3958979e4df7d857c4422b73f2392b79ed6d7ce
jonathan06112090 <120277969+jonathan06112090@users.noreply.github.com>
2024-06-10 09:40:17 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,13 +42,6 @@ export default function Home() {
 
     
   <MaxWidthWrapper className=' flex flex-col items-center justify-center text-center'>
-  <Image
-                    src='/pixelcut-export.jpg'
-                    alt='LOGO'
-                    width={540}
-                    height={500}
-                    className='object-none rounded-3xl '
-                 />
     <div className='mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50'>
         <p className='text-sm font-semibold text-gray-700'>
             Investify i Beta-stadiet
