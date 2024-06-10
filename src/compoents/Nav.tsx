a21d84d5fefaafb8ccc15d3a9aa5815a14ff3c06
jonathan06112090 <120277969+jonathan06112090@users.noreply.github.com>
2024-06-10 16:46:17 +0200

    Update Nav.tsx

--- src/compoents/Nav.tsx
+++ src/compoents/Nav.tsx
@@ -88,7 +88,7 @@ export default function Nav() {
                 {isDropdownOpen && (
                 <div className="absolute top-14 left-0 w-40 bg-white rounded-md shadow-lg transition-opacity duration-300 ease-in-out opacity-100">
                   <div className="py-1">
-                    <Link href="/Tilpasningl">
+                    <Link href="/Tilpasning">
                     <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer" onClick={closeDropdown}>
                       Tilpasning
                     </span>
